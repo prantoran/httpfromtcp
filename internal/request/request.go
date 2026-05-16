ca697555f0cd2c3b749134ad7706e5c4be28ec24
Pinku Deb Nath <prantoran@gmail.com>
2026-05-16 07:52:22 +0600

    Init tests for parsing request lines

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,1 +1,22 @@
 package request
+
+import (
+	"fmt"
+	"io"
+)
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+var ERROR_BAD_START_LINE = fmt.Errorf("Bad start line")
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	return nil, ERROR_BAD_START_LINE
+}
